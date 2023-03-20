b7ec39bc27e742dc11c2244d73d4572bcd3f12ba
yeonseong <yeonseong.dev@gmail.com>
2023-03-21 06:03:06 +0900

    FEAT: TGAColor in hpp

--- tgaimage/tgaimgae.hpp
+++ tgaimage/tgaimgae.hpp
@@ -2,7 +2,7 @@
 #define TGAIMGAE_HPP
 
 #include <fstream>
-typedef struct TGA_Header {
+typedef struct TGAHeader {
   unsigned char id_length;
   unsigned char color_map_type;
   unsigned char data_type_code;
@@ -15,6 +15,43 @@ typedef struct TGA_Header {
   short height;
   unsigned char bits_per_pixel;
   unsigned char image_descriptor;
-} TGA_Header;
+} TGAHeader;
+
+typedef struct TGAColor {
+  union {
+    struct {
+      unsigned char b;
+      unsigned char g;
+      unsigned char r;
+      unsigned char a;
+    };
+    unsigned char raw[4];
+    unsigned int val;
+  };
+  unsigned int bytespp;
+
+  TGAColor() : val(0), bytespp(1) {}
+
+  TGAColor(unsigned char R, unsigned char G, unsigned char B, unsigned char A)
+      : b(B), g(G), r(R), a(A), bytespp(4) {}
+
+  TGAColor(unsigned int v, unsigned int bpp) : val(v), bytespp(bpp) {}
+
+  TGAColor(const TGAColor &c) : val(c.val), bytespp(c.bytespp) {}
+
+  TGAColor(const unsigned char *p, int bpp) : val(0), bytespp(bpp) {
+    for (int i = 0; i < bpp; i++) {
+      raw[i] = p[i];
+    }
+  }
+
+  TGAColor &operator=(const TGAColor &c) {
+    if (this != &c) {
+      bytespp = c.bytespp;
+      val = c.val;
+    }
+    return *this;
+  }
+} TGAColor;
 
 #endif
