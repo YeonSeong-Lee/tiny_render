f541c60f19993d7a916d81efae993dc3804fadcd
yeonseong <yeonseong.dev@gmail.com>
2023-03-21 05:13:20 +0900

    REFERENCE: C++ union

---
+++ tgaimage/reference.md
@@ -0,0 +1,1 @@
+[C++ union](https://smoothiecoding.kr/cpp-union/)
--- tgaimage/tgaimgae.hpp
+++ tgaimage/tgaimgae.hpp
@@ -6,15 +6,15 @@ typedef struct TGA_Header {
   unsigned char id_length;
   unsigned char color_map_type;
   unsigned char data_type_code;
-  unsigned char bits_per_pixel;
-  unsigned char image_descriptor;
-  unsigned char color_map_depth;
   short color_map_origin;
   short color_map_length;
+  unsigned char color_map_depth;
   short x_origin;
   short y_origin;
   short width;
   short height;
+  unsigned char bits_per_pixel;
+  unsigned char image_descriptor;
 } TGA_Header;
 
 #endif
