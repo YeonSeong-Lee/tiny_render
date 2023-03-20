afd40fe8c09bd6c238762780c738a44c8affcbac
yeonseong <yeonseong.dev@gmail.com>
2023-03-21 06:07:27 +0900

    FEAT: TGAImgae in hpp

--- tgaimage/tgaimgae.hpp
+++ tgaimage/tgaimgae.hpp
@@ -54,4 +54,37 @@ typedef struct TGAColor {
   }
 } TGAColor;
 
+class TGAImage {
+ public:
+  enum Format { GRAYSCALE = 1, RGB = 3, RGBA = 4 };
+  TGAImage();
+  TGAImage(int w, int h, int bpp);
+  TGAImage(const TGAImage &img);
+  ~TGAImage();
+  TGAImage &operator=(const TGAImage &img);
+
+  bool read_tga_file(const char *filename);
+  bool write_tga_file(const char *filename, bool rle = true);
+  bool flip_horizontally();
+  bool flip_vertically();
+  bool scale(int w, int h);
+
+  TGAColor get(int x, int y);
+  bool set(int x, int y, TGAColor c);
+  int get_width();
+  int get_height();
+  int get_bytespp();
+  unsigned char *buffer();
+  void clear();
+
+ protected:
+  unsigned char *data;
+  int width;
+  int height;
+  int bytespp;
+
+  bool load_rle_data(std::ifstream &in);
+  bool unload_rle_data(std::ofstream &out);
+}
+
 #endif
