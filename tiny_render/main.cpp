ea4b64893386b9871d2ce43e8b80da335091b404
YeonSeong-Lee <yeonseong.dev@gmail.com>
2023-03-26 10:00:44 +0900

    FEAT: fifth attempt

--- tiny_render/main.cpp
+++ tiny_render/main.cpp
@@ -6,23 +6,30 @@ const TGAColor red = TGAColor(255, 0, 0, 255);
 void draw_line(int x0, int y0, int x1, int y1, TGAImage& image,
                TGAColor color) {
   bool steep = false;
-  if (std::abs(x0 - x1) <
-      std::abs(y0 - y1)) {  // if the line is steep, we transpose the image
+  if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
     std::swap(x0, y0);
     std::swap(x1, y1);
     steep = true;
   }
-  if (x0 > x1) {  // make it left−to−right
+  if (x0 > x1) {
     std::swap(x0, x1);
     std::swap(y0, y1);
   }
+  int dx = x1 - x0;
+  int dy = y1 - y0;
+  int derror2 = std::abs(dy) * 2;
+  int error2 = 0;
+  int y = y0;
   for (int x = x0; x <= x1; x++) {
-    float t = (x - x0) / (float)(x1 - x0);
-    int y = y0 * (1. - t) + y1 * t;
     if (steep) {
-      image.set(y, x, color);  // if transposed, de−transpose
+      image.set(y, x, color);
     } else {
       image.set(x, y, color);
+    }
+    error2 += derror2;
+    if (error2 > dx) {
+      y += (y1 > y0 ? 1 : -1);
+      error2 -= dx * 2;
     }
   }
 }
