7e51553fb2aba2deed0fd83f79a80bf64a3c4bc6
yeonseong <yeonseong.dev@gmail.com>
2023-03-27 22:54:49 +0900

    FEAT: sort points

--- main.cpp
+++ main.cpp
@@ -41,6 +41,9 @@ void draw_line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color) {
 }
 
 void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
+  if (t0.y > t1.y) std::swap(t0, t1);
+  if (t0.y > t2.y) std::swap(t0, t2);
+  if (t1.y > t2.y) std::swap(t1, t2);
   draw_line(t0, t1, image, color);
   draw_line(t1, t2, image, color);
   draw_line(t2, t0, image, color);
