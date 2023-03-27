ca7ba306363223aef469dd817f70a78d93220110
yeonseong <yeonseong.dev@gmail.com>
2023-03-27 22:52:58 +0900

    WIP: triangle

--- main.cpp
+++ main.cpp
@@ -3,29 +3,30 @@
 #include "tgaimage.hpp"
 
 Model* model = NULL;
-const int width = 1024;
-const int height = 1024;
+const int width = 200;
+const int height = 200;
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
 
-void draw_line(int x0, int y0, int x1, int y1, TGAImage& image,
-               TGAColor color) {
+void draw_line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color) {
   bool steep = false;
-  if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
-    std::swap(x0, y0);
-    std::swap(x1, y1);
+  if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y)) {
+    std::swap(p0.x, p0.y);
+    std::swap(p1.x, p1.y);
     steep = true;
   }
-  if (x0 > x1) {
-    std::swap(x0, x1);
-    std::swap(y0, y1);
+  if (p0.x > p1.x) {
+    std::swap(p0.x, p1.x);
+    std::swap(p0.y, p1.y);
   }
-  int dx = x1 - x0;
-  int dy = y1 - y0;
+  int dx = p1.x - p0.x;
+  int dy = p1.y - p0.y;
   int derror2 = std::abs(dy) * 2;
   int error2 = 0;
-  int y = y0;
-  for (int x = x0; x <= x1; x++) {
+  int y = p0.y;
+  int tempDeltaY = p1.y > p0.y ? 1 : -1;
+  for (int x = p0.x; x <= p1.x; x++) {
     if (steep) {
       image.set(y, x, color);
     } else {
@@ -33,11 +34,17 @@ void draw_line(int x0, int y0, int x1, int y1, TGAImage& image,
     }
     error2 += derror2;
     if (error2 > dx) {
-      y += (y1 > y0 ? 1 : -1);
+      y += tempDeltaY;
       error2 -= dx * 2;
     }
   }
 }
+
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
+  draw_line(t0, t1, image, color);
+  draw_line(t1, t2, image, color);
+  draw_line(t2, t0, image, color);
+};
 
 int main(int argc, char** argv) {
   if (2 == argc) {
@@ -46,18 +53,14 @@ int main(int argc, char** argv) {
     model = new Model("obj/african_head.obj");
   }
   TGAImage image(width, height, TGAImage::RGB);
-  for (int i = 0; i < model->nfaces(); i++) {
-    std::vector<int> face = model->face(i);
-    for (int j = 0; j < 3; j++) {
-      Vec3f v0 = model->vert(face[j]);
-      Vec3f v1 = model->vert(face[(j + 1) % 3]);
-      int x0 = (v0.x + 1.) * width / 2.;
-      int y0 = (v0.y + 1.) * height / 2.;
-      int x1 = (v1.x + 1.) * width / 2.;
-      int y1 = (v1.y + 1.) * height / 2.;
-      draw_line(x0, y0, x1, y1, image, white);
-    }
-  }
+
+  Vec2i t0[3] = {Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80)};
+  Vec2i t1[3] = {Vec2i(180, 50), Vec2i(150, 1), Vec2i(70, 180)};
+  Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+  triangle(t0[0], t0[1], t0[2], image, red);
+  triangle(t1[0], t1[1], t1[2], image, white);
+  triangle(t2[0], t2[1], t2[2], image, green);
+
   image.flip_vertically();  // i want to have the origin at the left bottom
                             // corner of the image
   image.write_tga_file("output.tga");
